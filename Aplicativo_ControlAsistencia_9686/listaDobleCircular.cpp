b7dcb16884c58a8943e34aec70f179e41471a13f
KevinColoma <87491549+KevinColoma@users.noreply.github.com>
2023-06-17 12:38:58 -0500

    Update listaDobleCircular.cpp

--- Aplicativo_ControlAsistencia_9686/listaDobleCircular.cpp
+++ Aplicativo_ControlAsistencia_9686/listaDobleCircular.cpp
@@ -6,166 +6,112 @@
  * Modified: sabado, 17 de junio de 2023 
  * Purpose: Modelado de Sistema para Control de Asistencia y su Implementacion  
  ********************************************************************************/
-
-#include "Nodo.h"
 #include "listaDobleCircular.h"
-#include <string>
 
-// Resto del código...
+template <class T>
+listaDobleCircular<T>::listaDobleCircular() : primero(nullptr), ultimo(nullptr) {}
 
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::listaDobleCircular()
-// Purpose:    Implementation of listaDobleCircular::listaDobleCircular()
-// Return:     
-////////////////////////////////////////////////////////////////////////
-
-listaDobleCircular::listaDobleCircular()
-{
-   nodo = nullptr;
+template <class T>
+listaDobleCircular<T>::~listaDobleCircular() {
+    // TODO: Implementar el destructor
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::~listaDobleCircular()
-// Purpose:    Implementation of listaDobleCircular::~listaDobleCircular()
-// Return:     
-////////////////////////////////////////////////////////////////////////
+template <class T>
+void listaDobleCircular<T>::insertar(T dato) {
+    Nodo<T>* nuevoNodo = new Nodo<T>(dato);
 
-listaDobleCircular::~listaDobleCircular()
-{
-   // TODO : implement
+    if (primero == nullptr) {
+        primero = nuevoNodo;
+        ultimo = nuevoNodo;
+        nuevoNodo->setSiguiente(nuevoNodo);
+        nuevoNodo->setAnterior(nuevoNodo);
+    } else {
+        nuevoNodo->setSiguiente(primero);
+        nuevoNodo->setAnterior(ultimo);
+        primero->setAnterior(nuevoNodo);
+        ultimo->setSiguiente(nuevoNodo);
+        primero = nuevoNodo;
+    }
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::insertar(Persona persona)
-// Purpose:    Implementation of listaDobleCircular::insertar()
-// Parameters:
-// - persona
-// Return:     void
-////////////////////////////////////////////////////////////////////////
+template <class T>
+void listaDobleCircular<T>::eliminar(T dato) {
+    if (primero == nullptr) {
+        return;  // La lista está vacía, no hay nada que eliminar
+    }
 
-void listaDobleCircular::insertar(Persona persona)
-{
-   // TODO : implement
+    Nodo<T>* actual = primero;
+
+    while (actual->getDato() != dato && actual->getSiguiente() != primero) {
+        actual = actual->getSiguiente();
+    }
+
+    if (actual->getDato() == dato) {
+        Nodo<T>* siguienteNodo = actual->getSiguiente();
+        Nodo<T>* anteriorNodo = actual->getAnterior();
+
+        siguienteNodo->setAnterior(anteriorNodo);
+        anteriorNodo->setSiguiente(siguienteNodo);
+
+        if (actual == primero) {
+            primero = siguienteNodo;
+        }
+        if (actual == ultimo) {
+            ultimo = anteriorNodo;
+        }
+
+        delete actual;
+    }
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::eliminar(Persona persona)
-// Purpose:    Implementation of listaDobleCircular::eliminar()
-// Parameters:
-// - persona
-// Return:     void
-////////////////////////////////////////////////////////////////////////
+template <class T>
+void listaDobleCircular<T>::imprimirDatos() {
+    if (primero == nullptr) {
+        return;  // La lista está vacía
+    }
 
-void listaDobleCircular::eliminar(Persona persona)
-{
-   // TODO : implement
+    Nodo<T>* actual = primero;
+
+    do {
+        // Imprimir el dato del nodo actual
+        // ...
+
+        actual = actual->getSiguiente();
+    } while (actual != primero);
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::imprimirDatos()
-// Purpose:    Implementation of listaDobleCircular::imprimirDatos()
-// Return:     void
-////////////////////////////////////////////////////////////////////////
+template <class T>
+int listaDobleCircular<T>::cantidadNodos() const {
+    if (primero == nullptr) {
+        return 0;  // La lista está vacía
+    }
 
-void listaDobleCircular::imprimirDatos(void)
-{
-   // TODO : implement
+    int contador = 0;
+    Nodo<T>* actual = primero;
+
+    do {
+        contador++;
+        actual = actual->getSiguiente();
+    } while (actual != primero);
+
+    return contador;
 }
 
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::crearNodo(Persona persona)
-// Purpose:    Implementation of listaDobleCircular::crearNodo()
-// Parameters:
-// - persona
-// Return:     Nodo
-////////////////////////////////////////////////////////////////////////
+template <class T>
+T listaDobleCircular<T>::buscarPorCedula(std::string cedula) {
+    if (primero == nullptr) {
+        return T{};  // La lista está vacía, retornar un valor por defecto del tipo T
+    }
 
-Nodo listaDobleCircular::crearNodo(Persona persona)
-{
-   // TODO : implement
-}
+    Nodo<T>* actual = primero;
 
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::cantidadNodos()
-// Purpose:    Implementation of listaDobleCircular::cantidadNodos()
-// Return:     int
-////////////////////////////////////////////////////////////////////////
+    do {
+        if (actual->getDato().getCedula() == cedula) {
+            return actual->getDato();
+        }
 
-int listaDobleCircular::cantidadNodos(void)
-{
-   // TODO : implement
-}
+        actual = actual->getSiguiente();
+    } while (actual != primero);
 
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::agregarPersona(Persona persona)
-// Purpose:    Implementation of listaDobleCircular::agregarPersona()
-// Parameters:
-// - persona
-// Return:     void
-////////////////////////////////////////////////////////////////////////
-
-void listaDobleCircular::agregarPersona(Persona persona)
-{
-   // TODO : implement
-}
-
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::buscarPorCedula(std::string cedula)
-// Purpose:    Implementation of listaDobleCircular::buscarPorCedula()
-// Parameters:
-// - cedula
-// Return:     Persona
-////////////////////////////////////////////////////////////////////////
-
-Persona listaDobleCircular::buscarPorCedula(std::string cedula)
-{
-   // TODO : implement
-}
-
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::getUltimo()
-// Purpose:    Implementation of listaDobleCircular::getUltimo()
-// Return:     Nodo
-////////////////////////////////////////////////////////////////////////
-
-Nodo listaDobleCircular::getUltimo(void)
-{
-   return ultimo;
-}
-
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::setUltimo(Nodo newUltimo)
-// Purpose:    Implementation of listaDobleCircular::setUltimo()
-// Parameters:
-// - newUltimo
-// Return:     void
-////////////////////////////////////////////////////////////////////////
-
-void listaDobleCircular::setUltimo(Nodo newUltimo)
-{
-   ultimo = newUltimo;
-}
-
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::getPrimero()
-// Purpose:    Implementation of listaDobleCircular::getPrimero()
-// Return:     Nodo
-////////////////////////////////////////////////////////////////////////
-
-Nodo listaDobleCircular::getPrimero(void)
-{
-   return primero;
-}
-
-////////////////////////////////////////////////////////////////////////
-// Name:       listaDobleCircular::setPrimero(Nodo newPrimero)
-// Purpose:    Implementation of listaDobleCircular::setPrimero()
-// Parameters:
-// - newPrimero
-// Return:     void
-////////////////////////////////////////////////////////////////////////
-
-void listaDobleCircular::setPrimero(Nodo newPrimero)
-{
-   primero = newPrimero;
+    return T{};  // No se encontró el dato, retornar un valor por defecto del tipo T
 }
